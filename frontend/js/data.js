a3a8058e7792c1ef2401c20c6753f6f251831a0c
Aaron Cabling <aaron.cblg@gmail.com>
2026-02-02 18:36:32 +0100

    changement noms des fichiers

--- frontend/js/data.js
+++ frontend/js/data.js
@@ -5,13 +5,13 @@ const games = [
     title: "BRAVEZ <br />LA GRANDE MURAILLE",
     subtitle:
       "Gauche, milieu, droite : choisissez votre voie sur la plus grande merveille du monde. Évitez les obstacles, testez vos réflexes et établissez un score digne de l'Empereur.",
-    img: "assets/img/samurai.png",
+    img: "assets/img/defi-de-lempereur-hero.png",
     linkPlay: "#",
     linkGh: "https://github.com/Rano-a/3-games-hub",
     score: 0,
     theme: "theme-samurai",
-    bg: "assets/img/61126.jpg",
-    logo: "assets/img/logogame1.png",
+    bg: "assets/img/defi-de-lempereur-bg.jpg",
+    logo: "assets/img/defi-de-lempereur-logo.png",
   },
   {
     /* Thème NEON */
@@ -19,13 +19,13 @@ const games = [
     title: "UN CLIC <br />UNE RÉACTION </br />UN CHAOS TOTAL",
     subtitle:
       "Déclenchez l'étincelle parfaite dans un monde de néons. Une seule chance pour créer la réaction en chaîne la plus massive et pulvériser vos scores.",
-    img: "assets/img/neon-personnage.png",
+    img: "assets/img/neon-pop-hero.png",
     linkPlay: "#",
     linkGh: "https://github.com/Rano-a/3-games-hub",
     score: 0,
     theme: "theme-neon",
-    bg: "assets/img/neon-bg.webp",
-    logo: "assets/img/logogame2.png",
+    bg: "assets/img/neon-pop-bg.webp",
+    logo: "assets/img/neon-pop-logo.png",
   },
   {
     /* Thème STICKY */
@@ -33,12 +33,12 @@ const games = [
     title: "DEFIEZ <br />LA GRAVITE",
     subtitle:
       "Accrochez-vous, visez haut et ne lâchez rien. Maîtrisez l'art de la rotation pour grimper toujours plus loin dans ce défi vertical infini.",
-    img: "assets/img/climber.png",
+    img: "assets/img/sticky-climber-hero.png",
     linkPlay: "#",
     linkGh: "https://github.com/Rano-a/3-games-hub",
     score: 0,
     theme: "theme-sticky",
-    bg: "assets/img/moutain-bg.jpg",
-    logo: "assets/img/logogame3.png",
+    bg: "assets/img/sticky-climber-bg.jpg",
+    logo: "assets/img/sticky-climber-logo.png",
   },
 ];
